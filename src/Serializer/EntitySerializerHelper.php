9ce868f99480547811ef085af41430baa460d030
MartkCz <martk494@gmail.com>
2024-12-01 20:09:58 +0100

    feat: add Serializer helper

---
+++ src/Serializer/EntitySerializerHelper.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace Shredio\Core\Serializer;
+
+use Cycle\ORM\ORMInterface;
+use Spiral\Core\Attribute\Singleton;
+
+#[Singleton]
+final class EntitySerializerHelper
+{
+
+	public function __construct(
+		private readonly ORMInterface $orm,
+	)
+	{
+	}
+
+	/**
+	 * @template T of object
+	 * @param class-string<T> $entity
+	 * @return T|null
+	 */
+	public function tryFindEntity(string $entity, mixed $value): ?object
+	{
+		if ($value === null) {
+			return null;
+		}
+
+		if (is_a($value, $entity)) {
+			return $value;
+		}
+
+		return $this->orm->getRepository($this->orm->resolveRole($entity))->findByPK($value);
+	}
+
+}
