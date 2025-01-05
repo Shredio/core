438c9cf7ee6490efadc972a326e6f8d72b8291c2
MartkCz <martk494@gmail.com>
2025-01-05 22:36:03 +0100

    feat: add support for parametrized collection endpoints
    Introduce a new `$isParametrized` property to support endpoints with dynamic parameters. Includes a getter method and updates the constructor to initialize this property. This enhances flexibility for defining parameterized collection routes.

--- src/Rest/Attribute/ReadCollectionEndpoint.php
+++ src/Rest/Attribute/ReadCollectionEndpoint.php
@@ -8,6 +8,20 @@ use Attribute;
 final readonly class ReadCollectionEndpoint extends Endpoint
 {
 
+	public function __construct(
+		?string $route = null,
+		array $groups = [],
+		private bool $isParametrized = false,
+	)
+	{
+		parent::__construct($route, $groups);
+	}
+
+	public function isParametrized(): bool
+	{
+		return $this->isParametrized;
+	}
+
 	public function getMethods(): array
 	{
 		return ['GET'];
