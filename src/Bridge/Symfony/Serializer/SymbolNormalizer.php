abbbe3a04b442e4fd043c575528dfbcba054a1eb
MartkCz <martk494@gmail.com>
2025-01-05 16:52:20 +0100

    feat: add denormalization support to SymbolNormalizer
    Implemented the DenormalizerInterface and added methods to handle denormalization of Symbol objects. Ensures compatibility with string data and proper type checks for Symbol::class during deserialization.

--- src/Bridge/Symfony/Serializer/SymbolNormalizer.php
+++ src/Bridge/Symfony/Serializer/SymbolNormalizer.php
@@ -3,9 +3,17 @@
 namespace Shredio\Core\Bridge\Symfony\Serializer;
 
 use Shredio\Core\Struct\Symbol;
+use Symfony\Component\Serializer\Exception\BadMethodCallException;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Exception\ExtraAttributesException;
+use Symfony\Component\Serializer\Exception\InvalidArgumentException;
+use Symfony\Component\Serializer\Exception\LogicException;
+use Symfony\Component\Serializer\Exception\RuntimeException;
+use Symfony\Component\Serializer\Exception\UnexpectedValueException;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-final class SymbolNormalizer implements NormalizerInterface
+final class SymbolNormalizer implements NormalizerInterface, DenormalizerInterface
 {
 
 	/**
@@ -29,6 +37,27 @@ final class SymbolNormalizer implements NormalizerInterface
 	public function supportsNormalization(mixed $data, ?string $format = null, array $context = []): bool
 	{
 		return $data instanceof Symbol;
+	}
+
+	/**
+	 * @param mixed[] $context
+	 */
+	public function denormalize(mixed $data, string $type, ?string $format = null, array $context = []): mixed
+	{
+		return new Symbol($data);
+	}
+
+	/**
+	 * @param mixed[] $context
+	 */
+	public function supportsDenormalization(
+		mixed $data,
+		string $type,
+		?string $format = null,
+		array $context = [],
+	): bool
+	{
+		return $type === Symbol::class && is_string($data);
 	}
 
 	/**
