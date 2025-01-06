9ef6b7a982025f23831a9db6e1dbefec808f4891
MartkCz <martk494@gmail.com>
2025-01-06 22:18:44 +0100

    feat: add permission checks to UserContext interface
    Introduced `isGranted` and `requirePermission` methods to the `UserContext` interface and implemented them in `SymfonyUserContext`. These methods allow for dynamic access control checks, with `requirePermission` supporting custom HTTP status codes in exception handling. This change enhances flexibility and security in permission management.

--- src/Bridge/Symfony/Security/SymfonyUserContext.php
+++ src/Bridge/Symfony/Security/SymfonyUserContext.php
@@ -2,10 +2,12 @@
 
 namespace Shredio\Core\Bridge\Symfony\Security;
 
+use Shredio\Core\Environment\AppEnvironment;
 use Shredio\Core\Security\AccountId;
-use Shredio\Core\Security\UserEntity;
 use Shredio\Core\Security\UserContext;
+use Shredio\Core\Security\UserEntity;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 final readonly class SymfonyUserContext implements UserContext
@@ -13,8 +15,25 @@ final readonly class SymfonyUserContext implements UserContext
 
 	public function __construct(
 		private Security $security,
+		private AppEnvironment $appEnv,
 	)
 	{
+	}
+
+	public function isGranted(string $attribute, ?object $subject = null): bool
+	{
+		return $this->security->isGranted($attribute, $subject);
+	}
+
+	public function requirePermission(string $attribute, ?object $subject = null, ?int $statusCode = null): void
+	{
+		if (!$this->security->isGranted($attribute, $subject)) {
+			if ($statusCode === null || $statusCode === 403) {
+				throw new AccessDeniedException();
+			}
+
+			throw new HttpException($statusCode, $this->appEnv->isDebugMode() ? 'Access Denied.' : '');
+		}
 	}
 
 	public function getUserId(): AccountId
@@ -29,7 +48,7 @@ final readonly class SymfonyUserContext implements UserContext
 
 	public function getUser(): UserEntity
 	{
-		return $this->getUserOrNull() ?? throw new AccessDeniedException('User is not authenticated.');
+		return $this->getUserOrNull() ?? throw new AccessDeniedException();
 	}
 
 	public function getUserOrNull(): ?UserEntity
--- src/Security/UserContext.php
+++ src/Security/UserContext.php
@@ -5,6 +5,10 @@ namespace Shredio\Core\Security;
 interface UserContext
 {
 
+	public function isGranted(string $attribute, ?object $subject = null): bool;
+
+	public function requirePermission(string $attribute, ?object $subject = null, ?int $statusCode = null): void;
+
 	public function getUserId(): AccountId;
 
 	public function getUserIdOrNull(): ?AccountId;
