2d4ec20019348defbac123587138cffc4180395e
MartkCz <martk494@gmail.com>
2025-03-05 13:53:20 +0100

    fix: extend correct class

--- src/Bridge/Doctrine/Type/DateTimeImmutablePrimaryType.php
+++ src/Bridge/Doctrine/Type/DateTimeImmutablePrimaryType.php
@@ -4,11 +4,11 @@ namespace Shredio\Core\Bridge\Doctrine\Type;
 
 use DateTimeImmutable;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Types\DateImmutableType;
+use Doctrine\DBAL\Types\DateTimeImmutableType;
 use Doctrine\DBAL\Types\Exception\InvalidFormat;
 use Shredio\Core\Bridge\Doctrine\DateTime\DateTimeImmutablePrimary;
 
-final class DateTimeImmutablePrimaryType extends DateImmutableType
+final class DateTimeImmutablePrimaryType extends DateTimeImmutableType
 {
 
 	public const string Name = 'datetime_immutable_primary';
