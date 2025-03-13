aef2335191cb3f2a3da56e50bed1ce52185a2376
MartkCz <martk494@gmail.com>
2025-03-13 17:08:18 +0100

    feat: make createClient non-static

--- src/Bridge/Symfony/Test/WebTestTrait.php
+++ src/Bridge/Symfony/Test/WebTestTrait.php
@@ -28,23 +28,9 @@ trait WebTestTrait // @phpstan-ignore-line
 	 * @param array $options An array of options to pass to the createKernel method
 	 * @param array $server  An array of server parameters
 	 */
-	protected static function createClient(array $options = [], array $server = []): KernelBrowser
+	protected function createClient(array $options = [], array $server = []): KernelBrowser
 	{
-		if (static::$booted) {
-			throw new \LogicException(\sprintf('Booting the kernel before calling "%s()" is not supported, the kernel should only be booted once.', __METHOD__));
-		}
-
-		$kernel = static::bootKernel($options);
-
-		try {
-			$client = $kernel->getContainer()->get('test.client');
-		} catch (ServiceNotFoundException) {
-			if (class_exists(KernelBrowser::class)) {
-				throw new \LogicException('You cannot create the client used in functional tests if the "framework.test" config is not set to true.');
-			}
-			throw new \LogicException('You cannot create the client used in functional tests if the BrowserKit component is not available. Try running "composer require symfony/browser-kit".');
-		}
-
+		$client = TestHelper::getClient($this->getKernel());
 		$client->setServerParameters($server);
 
 		/** @var KernelBrowser */
