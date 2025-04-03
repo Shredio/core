5491cbfa26e7b453f650142b7d075d80c4fd4228
MartkCz <martk494@gmail.com>
2025-04-03 11:18:37 +0200

    chore: simplify kernel, remove SlimKernel

--- src/Bridge/Symfony/Kernel/ConfigurableKernel.php
+++ src/Bridge/Symfony/Kernel/ConfigurableKernel.php
@@ -2,6 +2,11 @@
 
 namespace Shredio\Core\Bridge\Symfony\Kernel;
 
+use Shredio\Core\Path\Directories;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
 trait ConfigurableKernel // @phpstan-ignore-line
 {
 
@@ -21,9 +26,44 @@ trait ConfigurableKernel // @phpstan-ignore-line
 
 		$container = $this->getContainer();
 
+		$this->startup();
+
+		$this->services['directories'] ??= new Directories([
+			Directories::Root => $this->getProjectDir(),
+			Directories::Cache => $this->getCacheDir(),
+			Directories::Log => $this->getLogDir(),
+			...$this->getCustomDirectories(),
+		]);
+
 		foreach ($this->services as $id => $service) {
 			$container->set($id, $service);
 		}
+	}
+
+	protected function startup(): void
+	{
+		// This method can be overridden in the child class to perform any startup tasks.
+	}
+
+	/**
+	 * @return array<string, string>
+	 */
+	protected function getCustomDirectories(): array
+	{
+		return [];
+	}
+
+	public function handle(
+		Request $request,
+		int $type = HttpKernelInterface::MAIN_REQUEST,
+		bool $catch = true,
+	): Response
+	{
+		if ($request->getPathInfo() === '/healthz') {
+			return new Response('OK');
+		}
+
+		return parent::handle($request, $type, $catch);
 	}
 
 }
